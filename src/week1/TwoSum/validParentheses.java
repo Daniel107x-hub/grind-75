aa3bc464104a394a4a3a0b3f7a6e91c7f797fd8a
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-20 09:52:58 -0600

    Added valid parentheses problem

---
+++ src/week1/TwoSum/validParentheses.java
@@ -0,0 +1,26 @@
+package week1.TwoSum;
+
+import java.util.Stack;
+
+public class validParentheses {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean isValid(String s){
+        if(s.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for(Character c : s.toCharArray()){
+            if( c == '(' || c == '[' || c == '{') stack.push(c);
+            else{
+                if(!stack.isEmpty() && (
+                        c == ')' && stack.peek() == '(' ||
+                        c == ']' && stack.peek() == '[' ||
+                        c == '}' && stack.peek() == '{'
+                )) stack.pop();
+                else return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
