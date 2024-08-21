5e46f56820d20d170ce4320bd7ccf0be7f179508
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-21 07:43:51 -0600

    Added merge lists problem

---
+++ src/week1/TwoSum/mergeTwoLists.java
@@ -0,0 +1,44 @@
+package week1.TwoSum;
+
+public class mergeTwoLists {
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        if(list1 == null && list2 == null) return null;
+        if(list1 == null) return list2;
+        if(list2 == null) return list1;
+        ListNode head, tail = null;
+        if(list1.val < list2.val){
+            head = list1;
+            list1 = list1.next;
+        }else{
+            head = list2;
+            list2 = list2.next;
+        }
+        tail = head;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+            }else{
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = list1 == null ? list2 : list1;
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
+
+
