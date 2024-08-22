4b6c8e3ca542f872c1c5a3128fe83913c729619b
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-22 09:00:51 -0600

    Added invert binary tree problem

---
+++ src/week1/TwoSum/InvertBinaryTree.java
@@ -0,0 +1,32 @@
+package week1.TwoSum;
+
+public class InvertBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(4, new TreeNode(2), new TreeNode(3));
+        root = invertTree(root);
+        System.out.println("Hello");
+    }
+
+    public static TreeNode invertTree(TreeNode root){
+        if(root == null) return null;
+        TreeNode tmp = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(tmp);
+        return root;
+    }
+
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
