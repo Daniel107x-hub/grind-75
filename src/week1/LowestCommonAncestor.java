9e635bde1b7d579f5eda9a62b1eb247f66a00ddd
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-08-27 15:12:10 -0600

    Added lowest common ancestor problem

---
+++ src/week1/LowestCommonAncestor.java
@@ -0,0 +1,32 @@
+package week1;
+
+public class LowestCommonAncestor {
+    public static void main(String[] args) {
+        TreeNode q = new TreeNode(4, new TreeNode(3), new TreeNode(5));
+        TreeNode p = new TreeNode(2, new TreeNode(0), q);
+        TreeNode root = new TreeNode(6, p, new TreeNode(8, new TreeNode(7), new TreeNode(9)));
+        System.out.println(lca(root, p, q).val);
+    }
+
+    public static TreeNode lca(TreeNode root, TreeNode p, TreeNode q){
+        TreeNode max = p.val > q.val ? p : q;
+        TreeNode min = p.val < q.val ? p : q;
+        while(true){
+            if(min.val <= root.val && max.val >= root.val) return root;
+            if(min.val > root.val) root = root.right;
+            else root = root.left;
+        }
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+        TreeNode(int x, TreeNode left, TreeNode right){
+            val = x;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
