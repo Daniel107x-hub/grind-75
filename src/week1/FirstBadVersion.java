161c521eaea46444c555187bc7c53da7ad521b6c
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-09-02 11:27:55 -0600

    Added first bad note and ransom note problems

---
+++ src/week1/FirstBadVersion.java
@@ -0,0 +1,25 @@
+package week1;
+
+public class FirstBadVersion {
+    private static final int BAD_VERSION = 1702766719;
+
+    public static void main(String[] args) {
+        System.out.println(firstBadVersion(2126753390));
+    }
+
+    public static int firstBadVersion(int n){
+        int left = 1;
+        int right = n;
+        while(left < right){
+            int middle = (int)(((long)left + (long)right) / 2);
+            if(isBadVersion(middle)) right = middle;
+            else left = middle + 1;
+        }
+        return right;
+    }
+
+    public static boolean isBadVersion(int n){
+        if(n < 1) throw new IllegalArgumentException();
+        return n >= BAD_VERSION;
+    }
+}
---
+++ src/week1/RansomNote.java
@@ -0,0 +1,35 @@
+package week1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RansomNote {
+    public static void main(String[] args) {
+        System.out.println(canConstructArray("aac", "afa"));
+    }
+
+    public static boolean canConstruct(String ransomNote, String magazine){
+        if(magazine.length() < ransomNote.length()) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : magazine.toCharArray()) map.put(c, map.getOrDefault(c, 0) + 1);
+        for(char c : ransomNote.toCharArray()){
+            Integer value = map.get(c);
+            if(value == null) return false;
+            if(value == 1) map.remove(c);
+            else map.put(c, value - 1);
+        }
+        return true;
+    }
+
+    public static boolean canConstructArray(String ransomNote, String magazine){
+        if(magazine.length() < ransomNote.length()) return false;
+        int[] array = new int[26];
+        for(char c : magazine.toCharArray()) array[c - 'a']++;
+        for(char c : ransomNote.toCharArray()){
+            int value = array[c - 'a'];
+            if(value == 0) return false;
+            array[c - 'a']--;
+        }
+        return true;
+    }
+}
