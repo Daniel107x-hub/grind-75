cf63052fedf36a29e34875bd7bba85574b3016bd
Daniel107x-hub <DMejiaV310796@outlook.com>
2024-10-29 17:17:16 -0600

    Added max profit in job scheduling problem

--- src/week8/MaxProfitInJobScheduling.java
+++ src/week8/MaxProfitInJobScheduling.java
@@ -8,7 +8,7 @@ public class MaxProfitInJobScheduling {
         int[] startTime = {1,2,3,4,6};
         int[] endTime = {3,5,10,6,9};
         int[] profit = {20,20,100,70,60};
-        System.out.println(jobSchedulingWithMemo(startTime, endTime, profit));
+        System.out.println(jobSchedulingDP(startTime, endTime, profit));
     }
 
     public static int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
@@ -71,5 +71,44 @@ public class MaxProfitInJobScheduling {
     }
 
     // DP Version
+    public static int jobSchedulingDP(int[] startTime, int[] endTime, int[] profit) {
+        int[][] data = new int[startTime.length][3];
+        for(int i = 0 ; i < startTime.length ; i++){
+            data[i][0] = startTime[i];
+            data[i][1] = endTime[i];
+            data[i][2] = profit[i];
+        }
+        Arrays.sort(data, Comparator.comparingInt(a -> a[0]));
+        int[] cache = new int[startTime.length];
+        Arrays.fill(cache, -1);
+        return getMaxProfitDP(0, data, cache);
+    }
+
+    public static int getMaxProfitDP(int index, int[][] data, int[] cache){
+        if(index >= data.length || index < 0) return 0;
+        if(cache[index] != -1) return cache[index];
+        // No take the value
+        int notTake = getMaxProfitDP(index + 1, data, cache);
+        // Or take the value
+        int nextIndex = searchNextValidIndex(index, data);
+        int take = data[index][2] + getMaxProfitDP(nextIndex, data, cache);
+        // Compare
+        int maxProfit = Math.max(take, notTake);
+        cache[index] = maxProfit;
+        return maxProfit;
+    }
+
+    public static int searchNextValidIndex(int start, int[][] data){
+        int left = start;
+        int right = data.length - 1;
+        if(data[right][0] < data[start][1]) return -1;
+        while(left != right){
+            int mid = (left + right) / 2;
+            if(data[left][0] >= data[start][1]) right = left;
+            else if(data[mid][0] < data[start][1]) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
 
 }
